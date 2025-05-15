0a3b1156fee4e83ec855d87d2a7ee1681bc38dac
lebedev-023046 <lebedev.023046@gmail.com>
2025-05-15 09:30:56 +0300

    init: init repo

---
+++ src/shared/lib/hooks.js
@@ -0,0 +1,17 @@
+import { useRef, useEffect, useMemo } from 'react'
+import debounce from 'lodash/debounce'
+
+export const useDebounce = (callback, delay) => {
+	const ref = useRef()
+	useEffect(() => {
+		ref.current = callback
+	}, [callback])
+
+	const debouncedCallback = useMemo(() => {
+		const func = () => {
+			ref.current?.()
+		}
+		return debounce(func, delay)
+	}, [])
+	return debouncedCallback
+}
